1d2145e028d5ff9dd51cd735f58e4a958546032d
vaibhav <vaibhavktridhyatech@gmail.com>
2023-07-10 14:49:02 +0530

    Custom Logger

--- MagicVilla_VillaAPI/Program.cs
+++ MagicVilla_VillaAPI/Program.cs
@@ -1,11 +1,23 @@
+/*using Serilog;*/
+
+using MagicVilla_VillaAPI.Logging;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+/*Log.Logger = new LoggerConfiguration().MinimumLevel.Debug()
+    .WriteTo.File("log/villalogs.txt", rollingInterval: RollingInterval.Day).CreateLogger();
+
+builder.Host.UseSerilog();*/
+
+builder.Services.AddControllers(options => {
+    //options.ReturnHttpNotAcceptable = true;
+}).AddNewtonsoftJson().AddXmlDataContractSerializerFormatters();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton< ILogging, Logging > ();
 
 var app = builder.Build();
 
